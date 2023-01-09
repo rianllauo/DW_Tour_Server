0ff9717ed79e23f8041fbc1869d36ed812d66160
rianllauo <rianllauo@gmail.com>
2023-01-09 18:56:36 +0700

    update

--- dto/trip/trip_request.go
+++ dto/trip/trip_request.go
@@ -1,35 +1,39 @@
 package trip
 
-import "time"
+import (
+	"time"
+
+	"github.com/lib/pq"
+)
 
 type TripRequest struct {
 	Title string `json:"title" form:"title" gorm:"type: varchar(255)"`
 	// Country        string    `json:"country" gorm:"many2many:trip_country"`
-	CountryID      int       `json:"country_id" form:"country_id" gorm:"type: int"`
-	Accomodation   string    `json:"accomodation" form:"accomodation" gorm:"type: varchar(255)"`
-	Transportation string    `json:"transportation" form:"transportation" gorm:"type: varchar(255)"`
-	Eat            string    `json:"eat" form:"eat" gorm:"type: varchar(255)"`
-	Day            int       `json:"day" form:"day"`
-	Night          int       `json:"night" form:"night"`
-	DateTrip       time.Time `json:"date_trip" form:"date_trip"`
-	Price          int       `json:"price" form:"price"`
-	Quota          int       `json:"quota" form:"quota"`
-	Description    string    `json:"description" form:"description"`
-	Image          []string    `json:"image" form:"image" gorm:"type: varchar(255)"`
+	CountryID      int            `json:"country_id" form:"country_id" gorm:"type: int"`
+	Accomodation   string         `json:"accomodation" form:"accomodation" gorm:"type: varchar(255)"`
+	Transportation string         `json:"transportation" form:"transportation" gorm:"type: varchar(255)"`
+	Eat            string         `json:"eat" form:"eat" gorm:"type: varchar(255)"`
+	Day            int            `json:"day" form:"day"`
+	Night          int            `json:"night" form:"night"`
+	DateTrip       time.Time      `json:"date_trip" form:"date_trip"`
+	Price          int            `json:"price" form:"price"`
+	Quota          int            `json:"quota" form:"quota"`
+	Description    string         `json:"description" form:"description"`
+	Image          pq.StringArray `json:"image" form:"image" gorm:"type: varchar(255)"`
 }
 
 type UpdateTripRequest struct {
 	Title string `json:"title" form:"title" gorm:"type: varchar(255)"`
 	// Country        string    `json:"country" gorm:"many2many:trip_country"`
-	CountryID      int       `json:"country_id" form:"country_id" gorm:"type: int"`
-	Accomodation   string    `json:"accomodation" form:"accomodation" gorm:"type: varchar(255)"`
-	Transportation string    `json:"transportation" form:"transportation" gorm:"type: varchar(255)"`
-	Eat            string    `json:"eat" form:"eat" gorm:"type: varchar(255)"`
-	Day            int       `json:"day" form:"day"`
-	Night          int       `json:"night" form:"night"`
-	DateTrip       time.Time `json:"date_trip" form:"date_trip"`
-	Price          int       `json:"price" form:"price"`
-	Quota          int       `json:"quota" form:"quota"`
-	Description    string    `json:"description" form:"description"`
-	Image          string    `json:"image" form:"image" gorm:"type: varchar(255)"`
+	CountryID      int            `json:"country_id" form:"country_id" gorm:"type: int"`
+	Accomodation   string         `json:"accomodation" form:"accomodation" gorm:"type: varchar(255)"`
+	Transportation string         `json:"transportation" form:"transportation" gorm:"type: varchar(255)"`
+	Eat            string         `json:"eat" form:"eat" gorm:"type: varchar(255)"`
+	Day            int            `json:"day" form:"day"`
+	Night          int            `json:"night" form:"night"`
+	DateTrip       time.Time      `json:"date_trip" form:"date_trip"`
+	Price          int            `json:"price" form:"price"`
+	Quota          int            `json:"quota" form:"quota"`
+	Description    string         `json:"description" form:"description"`
+	Image          pq.StringArray `json:"image" form:"image" gorm:"type: varchar(255)"`
 }
