76988542bac35a8cdc02b9c26c08e5d6f462d952
rianllauo <111066646+rianllauo@users.noreply.github.com>
2023-01-09 18:29:24 +0700

    update

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -13,13 +13,13 @@ var DB *gorm.DB
 func DatabaseInit() {
 	var err error
 	
-	var PGHOST = os.Getenv("DB_HOST")
-	var PGUSER = os.Getenv("DB_USER")
-	var PGPASSWORD = os.Getenv("DB_PASSWORD")
-	var PGDATABASE = os.Getenv("DB_NAME")
-	var PGPORT = os.Getenv("DB_PORT")
+	var DB_HOST = os.Getenv("DB_HOST")
+	var DB_USER = os.Getenv("DB_USER")
+	var DB_PASSWORD = os.Getenv("DB_PASSWORD")
+	var DB_NAME = os.Getenv("DB_NAME")
+	var DB_PORT = os.Getenv("DB_PORT")
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", PGHOST, PGUSER, PGPASSWORD, PGDATABASE, PGPORT)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
